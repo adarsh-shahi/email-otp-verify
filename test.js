c83a246494ba441adc2970644c8b89b0e86928be
adarsh-shahi <adarshshahi1009@gmail.com>
2023-05-08 19:24:08 +0530

    Add option to expire otp in minutes

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-(async () => {
-	const data = await sendOTP({ email: "adarshshahi2404@gmail.com" });
-	console.log(data);
-})();
+import { sendOTP, verifyOTP, resendOTP } from "./index.js";
+// (async () => {
+// 	const data = await sendOTP({ email: "adarshshahi2404@gmail.com", min: 2 });
+// 	console.log(data);
+// })();
 (async () => {
 	const data = await verifyOTP({
 		token:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkYXJzaHNoYWhpMjQwNEBnbWFpbC5jb20iLCJpYXQiOjE2ODI2MTc4NzAsImV4cCI6MTY4MjYyMTQ3MH0.PRZ1ZluWnspUDigpnC586JvTZxnMXFkln669cjmbDXw",
-		otp: "166101",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkYXJzaHNoYWhpMjQwNEBnbWFpbC5jb20iLCJpYXQiOjE2ODM1NTM3MDAsImV4cCI6MTY4MzU1MzgyMH0.BUaz1lWxFcFDYXl4pSBvMnbzGfszXFMef4alMuAkoJs",
+		otp: "920944",
 	});
 	console.log(data);
 })();
